6e22a414276614e097bbf2493a0e8eca1e78f058
zelecto <ca346364@gmail.com>
2024-04-20 17:28:11 -0500

    Avance de pedidos

--- lib/presentation/screens/pedidos/pedidos.dart
+++ lib/presentation/screens/pedidos/pedidos.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
+import 'package:cutap/config/Screeb/screen_size.dart';
 import 'package:cutap/presentation/screens/Widgets/widgets_reutilizables.dart';
 import 'package:flutter/material.dart';
+
 
 class PedidosScreen extends StatefulWidget {
   const PedidosScreen({super.key});
@@ -32,7 +36,9 @@ class _PedidosScreenState extends State<PedidosScreen> {
                   icon: Icon(Icons.access_time), label: "Pendiente"),
             ],
           ),
-          currentPageIndex != 0
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: currentPageIndex != 0
               ? const TextField(
                   decoration: InputDecoration(
                     hintText: 'Buscar...',
@@ -40,7 +46,15 @@ class _PedidosScreenState extends State<PedidosScreen> {
                   ),
                 )
               : const Text(""),
-          currentPageIndex == 0 ? const _MyCardPedido() : const Text("Destruir mundo")
+            ),
+          
+
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: currentPageIndex == 0
+                ?  _MyCardPedido()
+                : const Text("Destruir mundo"),
+          )
         ],
       ),
     );
@@ -48,11 +62,11 @@ class _PedidosScreenState extends State<PedidosScreen> {
 }
 
 class _MyCardPedido extends StatelessWidget {
-  const _MyCardPedido();
+  _MyCardPedido();
+  
 
   @override
   Widget build(BuildContext context) {
-    final double heightDisponible = MediaQuery.of(context).size.height;
     return Card(
       color: Colors.white,
       elevation: 5,
@@ -69,34 +83,36 @@ class _MyCardPedido extends StatelessWidget {
             ),
           ),
           Container(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            color: Colors.black,
-            height: heightDisponible * 0.25,
-            width: heightDisponible * 0.25,
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            
+            height: ScreenSize.screenHeight * 0.25,
             child: Row(
               children: [
                 IconButton(
                   icon: const Icon(
                     Icons.arrow_back_ios_new,
                   ),
-                  color: Colors.white,
-                  onPressed: () {},
+                  
+                  onPressed: () {
+                    _controller.previousPage (
+                        duration: Duration(milliseconds: 400),
+                        curve: Curves.decelerate);
+                  },
                 ),
-                //TODO Aqui la lista de las ventas 
-                const FractionallySizedBox(
-                 
-                  child: SizedBox(
-                    
-                    child: _DetallePedidoReelView()
-                  ),
-                ),
+                //TODO Aqui la lista de las ventas
+                 const _DetallePedidoReelView(),
                 const Spacer(),
                 IconButton(
                   icon: const Icon(
                     Icons.arrow_forward_ios_outlined,
                   ),
-                  color: Colors.white,
-                  onPressed: () {},
+                  
+                  onPressed: () {
+                    _controller.nextPage(
+                        duration: Duration(milliseconds: 400),
+                        curve: Curves.decelerate
+                    );
+                  },
                 ),
               ],
             ),
@@ -107,59 +123,113 @@ class _MyCardPedido extends StatelessWidget {
   }
 }
 
-
-class _DetallePedidoReelView extends StatelessWidget {
+class _DetallePedidoReelView extends StatefulWidget {
   const _DetallePedidoReelView();
+
+  @override
+  State<_DetallePedidoReelView> createState() => _DetallePedidoReelViewState();
+}
+final PageController _controller = PageController();
+class _DetallePedidoReelViewState extends State<_DetallePedidoReelView> {
+  int _currentPage = 0;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    // Iniciar el temporizador para cambiar automáticamente la página cada 3 segundos
+    _timer = Timer.periodic(Duration(seconds: 3), (timer) {
+      if (_currentPage < productos.length) {
+        // Cambiar a la siguiente página
+        _currentPage++;
+        _controller.animateToPage(_currentPage,
+            duration: Duration(milliseconds: 500), curve: Curves.ease);
+      } else {
+        // Si es la última página, volver a la primera página
+        _currentPage = 0;
+        _controller.animateToPage(_currentPage,
+            duration: Duration(milliseconds: 500), curve: Curves.ease);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 100,
-      height: 100,
-      color: Colors.red
-
-    );
+        width: ScreenSize.screenWidth * 0.66,
+        
+        child: PageView.builder(
+          controller: _controller,
+          itemCount: productos.length,
+          itemBuilder: (context, index) => Column(children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 2),
+              child: TextoConNegrita(texto: productos[index]["nombre"], fontSize: 20),
+            ),
+            Container(
+              padding: const EdgeInsets.all(2),
+              width: ScreenSize.screenWidth * 0.7,
+              height: ScreenSize.screenHeight * 0.15,
+              child: Image.network(
+                productos[index]["imgUrl"],
+                fit: BoxFit.cover, //Imagen adaptativa
+              ),
+            ),
+             Expanded(
+               child: Row(
+                  children: [
+               
+                    Text("Cantidad : ${productos[index]["cantidadVenta"]}"),
+                    const Spacer(),
+                    Text("Precio ${productos[index]["precio"]}"),
+                    
+                  ]
+                ),
+             ),
+          ]),
+        ));
   }
 }
-
 
 List<Map<String, dynamic>> productos = [
   {
     'nombre': 'Café Negro',
-    'imgUrl': 'https://www.portafolio.co/files/article_new_multimedia/uploads/2021/11/20/6199a09028f1d.jpeg',
+    'imgUrl':
+        'https://www.portafolio.co/files/article_new_multimedia/uploads/2021/11/20/6199a09028f1d.jpeg',
     'precio': 2000, // En pesos colombianos
     'cantidadVenta': 10,
     'subtotal': 20000,
   },
   {
     'nombre': 'Café con Leche',
-    'imgUrl': 'https://www.portafolio.co/files/article_new_multimedia/uploads/2021/11/20/6199a09028f1d.jpeg',
+    'imgUrl':
+        'https://www.portafolio.co/files/article_new_multimedia/uploads/2021/11/20/6199a09028f1d.jpeg',
     'precio': 2500,
     'cantidadVenta': 8,
     'subtotal': 20000,
   },
   {
     'nombre': 'Pan de Queso',
-    'imgUrl': 'https://www.portafolio.co/files/article_new_multimedia/uploads/2021/11/20/6199a09028f1d.jpeg',
+    'imgUrl':
+        'https://assets.elgourmet.com/wp-content/uploads/2023/10/shutterstock_1758025376-1-1024x683.jpg.webp',
     'precio': 1500,
     'cantidadVenta': 15,
     'subtotal': 22500,
   },
   {
     'nombre': 'Avena con Pasas',
-    'imgUrl': 'https://www.portafolio.co/files/article_new_multimedia/uploads/2021/11/20/6199a09028f1d.jpeg',
+    'imgUrl':
+        'https://www.portafolio.co/files/article_new_multimedia/uploads/2021/11/20/6199a09028f1d.jpeg',
     'precio': 3000,
     'cantidadVenta': 5,
     'subtotal': 15000,
   },
   {
     'nombre': 'Empanada',
-    'imgUrl': 'https://www.portafolio.co/files/article_new_multimedia/uploads/2021/11/20/6199a09028f1d.jpeg',
+    'imgUrl':
+        'https://www.portafolio.co/files/article_new_multimedia/uploads/2021/11/20/6199a09028f1d.jpeg',
     'precio': 2000,
     'cantidadVenta': 12,
     'subtotal': 24000,
   },
 ];
-
-
-

ccc8b66bd68da11bd09fa76e616a52ed20ab1e90
zelecto <ca346364@gmail.com>
2024-04-23 12:56:08 -0500

    arreglo en las rutas

--- lib/config/router/rutas.dart
+++ lib/config/router/rutas.dart
@@ -4,7 +4,7 @@ import 'package:cutap/presentation/screens/screens.dart';
 
 // GoRouter configuration
 final appRouter = GoRouter(
-  initialLocation: '/Pedidos2',
+  initialLocation: '/barraNavegacion',
   routes: [
     GoRoute(
       path: '/',

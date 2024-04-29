7b73f146caefff4ea070b18d27c1d9c62b19445a
zelecto <ca346364@gmail.com>
2024-04-29 17:49:23 -0500

    Finalizacion de dise;o de CupCar con datos interactivos

--- lib/presentation/screens/CupCar/cupcar_screen.dart
+++ lib/presentation/screens/CupCar/cupcar_screen.dart
@@ -1,114 +1,116 @@
 import 'package:cutap/config/Screeb/screen_size.dart';
+import 'package:cutap/entity/pedido/venta_producto.dart';
+import 'package:cutap/presentation/provider/pedido/detalles_pedido_provider.dart';
 import 'package:cutap/presentation/screens/Widgets/widgets_reutilizables.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
-class CupCarScreen extends StatefulWidget {
+class CupCarScreen extends ConsumerStatefulWidget {
   const CupCarScreen({super.key});
 
   @override
-  State<CupCarScreen> createState() => _CupCarScreenState();
+  CupCarScreenState createState() => CupCarScreenState();
 }
 
-class _CupCarScreenState extends State<CupCarScreen> {
+class CupCarScreenState extends ConsumerState<CupCarScreen> {
   @override
   Widget build(BuildContext context) {
+    final List<DetallePedido> listaDetalles = ref.watch(detallesPedidoProvider);
+    double totalPagar = 0;
+    listaDetalles.forEach((element) {
+      totalPagar += element.subPrecioCobro;
+    });
     return Scaffold(
-      appBar: crearAppbar(
-          "Realiza tus pedidos", const Icon(Icons.shopping_bag_outlined)),
-      body: CustomScrollView(
-        
-        slivers: [
-          SliverPadding(padding: const EdgeInsets.all(15),
-            sliver: SliverList(
-              delegate: SliverChildListDelegate([
-                const Center(
-                  child: TextoConNegrita(
-                    fontSize: 25,
-                    texto: "TU COMPRA",
+        appBar: crearAppbar(
+            "Consulta tu carrito", const Icon(Icons.shopping_bag_outlined)),
+        body: listaDetalles.isNotEmpty
+            ? CustomScrollView(
+                slivers: [
+                  SliverPadding(
+                    padding: const EdgeInsets.all(15),
+                    sliver: SliverList(
+                      delegate: SliverChildListDelegate([
+                        SizedBox(
+                          height: ScreenSize.screenHeight * 0.01,
+                        )
+                      ]),
+                    ),
                   ),
-                ),
-                const Divider(
-                  color: Colors.black38,
-                  thickness: 3,
-                ),
-              ]),
-            ),
-          ),
-
-          SliverGrid(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 1,
-              childAspectRatio: 3,
-              mainAxisSpacing: 20,
-                ),
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15),
-                  child: _MyCardPedido(
-                      "Empanada",
-                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGSZU9NZXEIFylmIrCbu7D6L9g8vxRAooAWw&usqp=CAU",
-                      10000,
-                      100000,
-                      100),
-                );
-              },
-              childCount: 5,
-            ),
-          ),
-          
-          SliverPadding(padding: const EdgeInsets.all(20),
-           sliver: SliverList(
-              delegate: SliverChildListDelegate([
-                Row(
-                  children: [
-                    FilledButton(
-                      onPressed: () {},
-                      child: const Text("RESERVAR"),
+                  SliverGrid(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1,
+                      childAspectRatio: 3,
+                      mainAxisSpacing: 20,
                     ),
-                    const Spacer(),
-                    FilledButton(
-                      onPressed: () {},
-                      child: const Text("PAGAR"),
+                    delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                        return Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: _MyCardPedido(
+                              detallePedido: listaDetalles[index],
+                            ));
+                      },
+                      childCount: listaDetalles.length,
                     ),
-                  ],
-                ),
-                
-              ]),
-            ),
-          )
-          
-        ],
-      ),
-    );
+                  ),
+                  SliverPadding(
+                    padding: const EdgeInsets.all(20),
+                    sliver: SliverList(
+                      delegate: SliverChildListDelegate([
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              const TextoConNegrita(texto: "Total", fontSize: 20),
+                              const Spacer(),
+                              TextoConNegrita(
+                                  texto: "$totalPagar", fontSize: 20),
+                              const Icon(Icons.attach_money)
+                            ],
+                          ),
+                        ),
+                        const Divider(
+                          height: 20,
+                          color: Colors.black38,
+                          thickness: 3,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              FilledButton(
+                                onPressed: () {},
+                                child: const Text("RESERVAR"),
+                              ),
+                              const Spacer(),
+                              FilledButton(
+                                onPressed: () {},
+                                child: const Text("PAGAR"),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ]),
+                    ),
+                  )
+                ],
+              )
+            : const CupCarVacioView());
   }
 }
 
-class _MyCardPedido extends StatelessWidget {
-  final String nameProduct;
-  final String imgUrl;
-  final double precioCobrar;
-  final double totalPagar;
-  final int cantidadVendida;
-  
+class _MyCardPedido extends ConsumerWidget {
+  final DetallePedido detallePedido;
 
-  const _MyCardPedido(
-    this.nameProduct, 
-    this.imgUrl, 
-    this.precioCobrar,
-    this.totalPagar, 
-    this.cantidadVendida
-  );
+  const _MyCardPedido({required this.detallePedido});
   TextStyle estiloTexto(Color color) {
     return TextStyle(fontSize: 18, color: color, fontWeight: FontWeight.w600);
   }
 
   @override
-  Widget build(BuildContext context) {
-    
+  Widget build(BuildContext context, WidgetRef ref) {
     return Container(
-      
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(15),
         color: Colors.white,
@@ -119,44 +121,100 @@ class _MyCardPedido extends StatelessWidget {
             blurRadius: 5,
             offset: const Offset(5, 5),
           ),
-        ], 
+        ],
       ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Stack(children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: SizedBox(
+                width: ScreenSize.screenWidth * 0.40,
+                height: ScreenSize.screenHeight * 0.15,
+                //TODO: Implementra imagenes
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.network(
+                      "https://i.ytimg.com/vi/m3acCpS4DJg/maxresdefault.jpg",
+                      fit: BoxFit.cover),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: ScreenSize.screenWidth * 0.40,
+              height: ScreenSize.screenHeight * 0.20,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextoConNegrita(
+                      texto: detallePedido.producto.nombre, fontSize: 25),
+                  Text("${detallePedido.cantidaVendida} U",
+                      style: estiloTexto(Colors.grey.shade600)),
+                  Row(
+                    children: [
+                      Text(
+                        "${detallePedido.subPrecioCobro} ",
+                        textAlign: TextAlign.center,
+                        style: estiloTexto(Colors.blue),
+                      ),
+                      const Icon(
+                        Icons.attach_money,
+                        color: Colors.blue,
+                        size: 20,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+        Positioned(
+          top: 0,
+          right: 0,
+          child: Container(
+            decoration: const BoxDecoration(
+                color: Colors.red,
+                borderRadius:
+                    BorderRadius.horizontal(left: Radius.circular(15))),
+            child: IconButton(
+                onPressed: () {
+                  ref
+                      .read(detallesPedidoProvider.notifier)
+                      .eliminarDetalle(detallePedido);
+                },
+                icon: const Icon(
+                  Icons.delete_outline,
+                  color: Colors.white,
+                )),
+          ),
+        )
+      ]),
+    );
+  }
+}
+
+class CupCarVacioView extends StatelessWidget {
+  const CupCarVacioView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(10),
+          Center(
             child: SizedBox(
-              width: ScreenSize.screenWidth*0.40,
-              height: ScreenSize.screenHeight*0.15,
-              child: Image.network(imgUrl, fit: BoxFit.cover),
+              width: ScreenSize.screenWidth * 0.6,
+              height: ScreenSize.screenHeight * 0.6,
+              child: Image.asset(
+                "assets/images/CupCarVacio.png",
+                fit: BoxFit.contain,
+              ),
             ),
           ),
-
-          SizedBox(
-            width: ScreenSize.screenWidth*0.40,
-            height: ScreenSize.screenHeight*0.20,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextoConNegrita(texto: nameProduct, fontSize: 25),
-                Text("15 U", 
-                  style: estiloTexto(Colors.grey.shade600)),
-                Row(
-                  children: [
-                    Text(
-                      "$precioCobrar ",
-                      textAlign: TextAlign.center,
-                      style: estiloTexto(Colors.blue),
-                    ),
-                    const Icon(Icons.attach_money,color: Colors.blue, size: 20,)
-                  ],
-                ),
-                //Pie de tarjeta
-                
-              ],
-            ),
-          )
+          const TextoConNegrita(texto: "Tu carrito esta vacio", fontSize: 40)
         ],
       ),
     );

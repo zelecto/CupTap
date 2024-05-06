f793bfdede810d9ad31d0b7ec103a3172578c05e
zelecto <ca346364@gmail.com>
2024-05-06 18:54:46 -0500

    cambios que no recuerdo

--- lib/presentation/screens/pedidos/pedidos_sreens.dart
+++ lib/presentation/screens/pedidos/pedidos_sreens.dart
@@ -20,7 +20,7 @@ class PedidosScreenState extends ConsumerState<PedidosScreen> {
   @override
   Widget build(BuildContext context) {
     //TODO IMPLEMENTAR USUARIO
-    final pedidoAsync = ref.watch(consultaPedidoProvider("1067593679"));
+    final pedidoAsync = ref.watch(consultaPedidoProvider("1003238420"));
     return Scaffold(
         appBar: crearAppbar("Pedidos", const Icon(Icons.shopping_bag_outlined)),
         body: pedidoAsync.when(
